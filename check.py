dad3ae4bf7e340ab7860800810bf7f4fb7bd2874
rudolf.ildefonso <rudolf.ildefonso@entropysolution.com>
2021-05-12 05:09:53 +0800

    Include initial SMTP setup

--- check.py
+++ check.py
@@ -1,9 +1,13 @@
+import os
 from requests import get
 
-URL = 'http://127.0.0.1:5000/error'
+URL = os.environ.get('HEALTHCHECK_URL', 'http://127.0.0.1:5000')
 
 
 def send_email(subject, body):
+    import smtplib
+    server = smtplib.SMTP(os.environ.get('HEALTHCHECK_ALERT_SMTP', 'localhost'))
+    server.sendmail(os.environ.get('HEALTHCHECK_ALERT_SENDER', 'admin@thisfish.co'), os.environ.get('HEALTHCHECK_ALERT_RECEPIENTS', 'app_support@thisfish.co'))
     print(f'subject: {subject}\nbody: {body}')
 
 
